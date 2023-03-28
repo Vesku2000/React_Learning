6a7c2fcfaf6f1139fea517b2762f66599b9330a2
Valtteri <102945073+Vesku2000@users.noreply.github.com>
2023-03-28 15:13:39 +0000

    navbar edited

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,16 @@ export default function App() {
   </div>;
 
   return <div style={{ padding: "15px" }}>
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
     <h1>Welcome, {name}!</h1>
     <p>Thanks for using <b>bddy</b>'s react + bootstrap template!</p>
     <hr />
@@ -43,16 +53,7 @@ export default function App() {
       </Button>
     </Form>
 
-    <Navbar bg="dark" variant="dark">
-      <Container>
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-        </Nav>
-      </Container>
-    </Navbar>
+
     <br />
 
     <hr />
