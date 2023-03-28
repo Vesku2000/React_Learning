57b6d11050c8ab7e29689741a103fa0479c6f02d
Valtteri <102945073+Vesku2000@users.noreply.github.com>
2023-03-28 15:18:40 +0000

    added 4 buttons

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ export default function App() {
   const [name, setName] = useState(null);
   const [nameFieldText, setNameFieldText] = useState("");
 
-  const [favoriteName, setFavoriteName] = useState(null);
+
 
   if (name == null) return <div style={{ padding: "15px" }}>
     <h1>Welcome, Guest!</h1>
@@ -35,56 +35,15 @@ export default function App() {
 
 
     <br />
+  <h1>Button examples</h1>
+    <hr />
+<button type="button" class="btn btn-primary">Primary</button>
+<button type="button" class="btn btn-secondary">Secondary</button>
+<button type="button" class="btn btn-success">Success</button>
+<button type="button" class="btn btn-danger">Danger</button>
 
     <hr />
-    {[false, 'sm', 'md', 'lg', 'xl', 'xxl'].map((expand) => (
-      <Navbar key={expand} bg="light" expand={expand} className="mb-3">
-        <Container fluid>
-          <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand>
-          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-          <Navbar.Offcanvas
-            id={`offcanvasNavbar-expand-${expand}`}
-            aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-            placement="end"
-          >
-            <Offcanvas.Header closeButton>
-              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                Offcanvas
-              </Offcanvas.Title>
-            </Offcanvas.Header>
-            <Offcanvas.Body>
-              <Nav className="justify-content-end flex-grow-1 pe-3">
-                <Nav.Link href="#action1">Home</Nav.Link>
-                <Nav.Link href="#action2">Link</Nav.Link>
-                <NavDropdown
-                  title="Dropdown"
-                  id={`offcanvasNavbarDropdown-expand-${expand}`}
-                >
-                  <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action4">
-                    Another action
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action5">
-                    Something else here
-                  </NavDropdown.Item>
-                </NavDropdown>
-              </Nav>
-              <Form className="d-flex">
-                <Form.Control
-                  type="search"
-                  placeholder="Search"
-                  className="me-2"
-                  aria-label="Search"
-                />
-                <Button variant="outline-success">Search</Button>
-              </Form>
-            </Offcanvas.Body>
-          </Navbar.Offcanvas>
-        </Container>
-      </Navbar>
-    ))}
-    <hr />
+    <h1>Color variants</h1>
     <Button variant="outline-primary">Primary</Button>{' '}
     <Button variant="outline-secondary">Secondary</Button>{' '}
     <Button variant="outline-success">Success</Button>{' '}
