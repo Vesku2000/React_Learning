22f3c381d5fdb7cc5c4bc2270e4c939c2c52e56e
Valtteri <102945073+Vesku2000@users.noreply.github.com>
2023-03-28 15:14:43 +0000

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ export default function App() {
   </div>;
 
   return <div style={{ padding: "15px" }}>
-    <Navbar bg="dark" variant="dark">
+    <Navbar bg="dark" variant="primary">
       <Container>
         <Navbar.Brand href="#home">Navbar</Navbar.Brand>
         <Nav className="me-auto">
@@ -32,26 +32,6 @@ export default function App() {
     <hr />
     <h4>Here's some bootstap examples!</h4>
     <hr />
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
 
 
     <br />
